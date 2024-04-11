4f369db4fca546083577b139c559319054b2d1cf
Morgan Douglas <mdouglas47@bloomberg.net>
2024-04-11 11:47:39 -0400

    Import for blob tables
    Signed-off-by: Morgan Douglas <mdouglas47@bloomberg.net>

--- db/import.c
+++ db/import.c
@@ -253,6 +253,7 @@ int bulk_import_data_load(ImportData *p_data)
     /* get stripe options */
     p_data->dtastripe = gbl_dtastripe;
     p_data->blobstripe = gbl_blobstripe;
+    p_data->filenames_provided = gbl_enable_bulk_import_different_tables;
 
     // p_data->n_index_files = p_data->n_index_genids;
     // p_data->index_files = malloc(sizeof(char *)*p_data->n_index_files);
@@ -305,7 +306,11 @@ int bulk_import_data_load(ImportData *p_data)
 
     /* get num indicies/blobs */
     p_data->num_index_genids = db->nix;
+    p_data->n_index_genids = p_data->num_index_genids;
+    p_data->index_genids = malloc(sizeof(unsigned long int)*p_data->n_index_genids);
     p_data->num_blob_genids = db->numblobs;
+    p_data->n_blob_genids = p_data->num_blob_genids;
+    p_data->blob_genids = malloc(sizeof(unsigned long int)*p_data->n_blob_genids);
 
     /* for each index, lookup version */
     for (i = 0; i < p_data->num_index_genids; ++i) {
